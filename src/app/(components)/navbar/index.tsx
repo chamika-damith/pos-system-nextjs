ec22023cc256a95da607ab770fad92ba81afeaab
chamika-damith <chamikadamith9@gmail.com>
2024-10-24 16:38:45 +0530

    create sidebar

--- src/app/(components)/navbar/index.tsx
+++ src/app/(components)/navbar/index.tsx
@@ -7,12 +7,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center w-full mb-7'>
-      <div className='lg:hidden flex justify-between items-center gap-5'>
+      <div className='flex justify-between items-center gap-5'>
         <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={() => { }}>
           <Menu className='w-4 h-4' />
         </button>
       </div>
-      <div className='relative lg:ml-72'>
+      <div className='relative '>
         <input type='search' placeholder='start type to search..' className='mr-2 pl-10 pr-2 py-1 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg  focus:outline-none focus:border-blue-500' />
         <button className='absolute inset-y-0 left-0 pl-3 flex items-center' onClick={() => { }}>
           <Search className='text-gray-500 ' size={20} />
--- src/app/DashboardWrapper.tsx
+++ src/app/DashboardWrapper.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from './(components)/navbar'
+import Sidebar from './(components)/sidebar'
 
 const DashboardWrapper = ({children} : {children:React.ReactNode}) => {
   return (
     <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      <Sidebar />
         <main className={`flex flex-col w-full h-full py-7 bg-gray-50 md:pl-24`}>
             <Navbar />
             {children}
